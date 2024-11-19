1e525b24fc84fbf0db767809e2486beb300a1f3c
liempot <leemagno9@gmail.com>
2024-11-19 08:01:45 +0800

    wow-quiz

---
+++ src/questions.jsx
@@ -0,0 +1,134 @@
+export const questions = [
+  {
+    question_aid: 1,
+    question_question: "Q1: Pogi ba ako?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Cguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 2,
+    question_question: "Q2: May pasok ba bukas?",
+    choices: [
+      {
+        choice: "Ayaw",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Cguro nga",
+        isCorrect: false,
+      },
+      {
+        choice: "Wala Syempre!",
+        isCorrect: true,
+      },
+    ],
+  },
+  {
+    question_aid: 3,
+    question_question: "Q3: Nag-toothbrush ka ba?",
+    choices: [
+      {
+        choice: "Meh",
+        isCorrect: false,
+      },
+      {
+        choice: "Hinde Lods",
+        isCorrect: false,
+      },
+      {
+        choice: "Cguro",
+        isCorrect: true,
+      },
+      {
+        choice: "Asa ka pa!",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 4,
+    question_question: "Q4: Papasok ba ako bukas?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Cguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Asa ka pa!",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 5,
+    question_question: "Q5: Makakapasa ba ako sa exam?",
+    choices: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Cguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Asa ka pa!",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    question_aid: 6,
+    question_question: "Q6: Uulan ba ako mamaya?",
+    choices: [
+      {
+        choice: "Nice",
+        isCorrect: false,
+      },
+      {
+        choice: "Wag po!",
+        isCorrect: true,
+      },
+      {
+        choice: "Cguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Nahhh!",
+        isCorrect: false,
+      },
+    ],
+  },
+];
